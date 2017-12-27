c8b1a2a39e0ec3dda2266e5768221b83e54c079d
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2017-12-28 00:50:42 +0800

    Initial finished files

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func randomFact(_ sender: Any) {
+        let array = ["""
+                        A single cloud can weight more
+                        than 1 million pounds.
+                        """,
+                     """
+                        A human will eat on average
+                        70 assorted insects
+                        and 10 spiders while sleeping.
+                        """,
+                     """
+                        James Buchanan, the 15th U.S.
+                        president continuously
+                        bought slaves with his own money
+                        in order to free them.
+                        """,
+                     """
+                        Men are 6 times more likely to
+                        be struck by lightning than women.
+                        """,
+                     """
+                        You cannot snore and dream at the same time.
+                        """]
+        let randomFact = Int(arc4random_uniform(UInt32(array.count)))
+        label.text = array[randomFact]
+    }
+    
 }
 
